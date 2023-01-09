c368da925a5d7c0c8ffb2dd50b9f180785148608
Иван Цыганов <zamegagurrendan@gmail.com>
2023-01-09 22:07:54 +0500

    2nd ver after review

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,10 +5,12 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
         return (
-                obj.author == request.user
-                or request.method in permissions.SAFE_METHODS)
+            obj.author == request.user
+            or request.method in permissions.SAFE_METHODS
+        )
